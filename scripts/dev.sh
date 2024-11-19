cbc34f6e899ef07f685b83a0519e536aa2478d0c
Shannon Code <genecyber@gmail.com>
2024-11-18 22:44:56 -0500

    should not change that file

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -9,4 +9,4 @@ npx concurrently --raw \
   "pnpm --dir packages/plugin-node dev -- $*" \
   "pnpm --dir packages/adapter-sqlite dev -- $*" \
   "pnpm --dir packages/adapter-postgres dev -- $*" \
-  "node -e \"setTimeout(() => process.exit(0), 5000)\" && pnpm --dir packages/agent dev -- $* --characters=\"characters/fusion.character.json,characters/shannonai.character.json\""
+  "node -e \"setTimeout(() => process.exit(0), 5000)\" && pnpm --dir packages/agent dev -- $*"
